da536f1895ca00db81616f3f5b54fce36999e914
rolita1007 <rolita1007@hotmail.com>
2018-01-05 10:49:23 -0500

    connected the eventlistener to it's cell

--- connect4.js
+++ connect4.js
@@ -4,11 +4,10 @@ class Connect4 {
     this.Columns = 7;
     this.selector = selector;
     this.createGrid();
-      
-     
+    this.setupEventListeners();  
   }
 
-    //Creating the grid
+  //Creating the grid
 
   createGrid() {
     const $board = $(this.selector);
@@ -17,10 +16,36 @@ class Connect4 {
         .addClass('row');
       for (let column = 0; column < this.Columns; column++) {
         const $column = $('<div>')
-          .addClass('column empty');
+          .addClass('column empty')
+          .attr('data-row', row)
+          .attr('data-column', column);
         $row.append($column);
       }
         $board.append($row);
     }
+  }
+
+  //Setting up the click event for every slot clicked
+
+  setupEventListeners() {
+    const $board = $(this.selector);
+
+    function findLastEmptyCell(column) {
+      const cells = $(`.column[data-column='${column}']`);
+      for (let i = cells.length - 1; i >= 0; i--) {
+        const $cell = $(cells[i]);
+        if ($cell.hasClass('empty')) {
+          return $cell;
+        }
+      }
+      return null;
+    }
+
+    $board.on('mouseenter', '.column.empty', function() {
+      const column = $(this).data('column');
+      const $lastEmptyCell = findLastEmptyCell(column);
+      $lastEmptyCell.addClass('red-chip');
+      console.log(column);
+    })
   }
 }
